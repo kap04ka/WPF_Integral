bcb7ee57126c21734db0cd1d3d976746790d7959
kap04ka <98656810+kap04ka@users.noreply.github.com>
2022-04-25 19:29:15 +0300

    Update MiddleRect.cs

--- Classes/MiddleRect.cs
+++ Classes/MiddleRect.cs
@@ -9,7 +9,7 @@ namespace WPF_Integral.Classes
 {
     public class MiddleRect : ICalculator
     {
-        public double Calculate(double steps, double start, double end, Func<double, double> func, out double t)
+        public double Calculate(int steps, double start, double end, Func<double, double> func, out double t)
         {
             if (steps <= 0) throw new ArgumentException("Incorrect input (steps must be >=1)");
             bool messedLims = false;
